4689dff3950ca7908689821ce160d225554b8335
Louiss01 <luisyfrancisco@gmail.com>
2026-02-02 20:20:10 +0100

    Basicas, nav, contadores y pruebas de redirección.

--- frontend-angular/src/app/app.routes.ts
+++ frontend-angular/src/app/app.routes.ts
@@ -2,9 +2,16 @@ import { Routes } from '@angular/router';
 // Importamos manualmente los componentes buscando la ruta exacta
 import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
+import { Conocenos } from './pages/conocenos/conocenos';
+import { Faq } from './pages/faq/faq';  
+
+//definimos las rutas
+
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home },
-  { path: 'login', component: Login }
+  { path: 'login', component: Login },
+  { path: 'conocenos', component: Conocenos },
+  { path: 'faq', component: Faq }
 ];
--- frontend-angular/src/app/pages/home/home.ts
+++ frontend-angular/src/app/pages/home/home.ts
@@ -7,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrl: './home.scss',
 })
 export class Home {
+  number: number = 0;
+  count: number = 0;
+  timer: any; 
+  mensajeMostrado: boolean = false; 
 
+  addOne() {
+    console.log('¡Botón clickeado!');
+    this.number++;
+
+    if (!this.mensajeMostrado) {
+      if (this.timer) {
+        clearTimeout(this.timer);
+      }
+
+      this.timer = setTimeout(() => {
+        alert('¿Te has cansado? Patético humano.');
+        this.mensajeMostrado = true; 
+      }, 10000); 
+    }
+
+    if (this.number === 10) {
+      alert('No iba en serio... ¿o sí?');
+    }
+    if (this.number === 20) {
+      alert('¡Vaya, te gusta hacer clics!');
+    }
+    if (this.number === 30) {
+      alert('¡Eso es mucha dedicación!');
+    }
+    if (this.number === 40) {
+      alert('¿Estás intentando romper el récord mundial de clics?');
+    }
+    if (this.number === 50) {
+      alert('¡Increíble! ¡50 clics! ¿Qué sigue, 100?');
+    }
+    if (this.number === 100) {
+      alert('¡Felicidades! ¡Has alcanzado los 100 clics! Eres un campeón del clic.');
+    }
+    if (this.number > 200) {
+      alert('¡Wow! ¡Más de 200 clics! ¿Estás seguro de que no tienes otra cosa que hacer?');
+    }
+    if (this.number > 500) {
+      alert('¡Increíble! ¡Más de 500 clics! ¿Estás tratando de comunicarte con los extraterrestres a través de clics?');
+    }
+    if (this.number > 1000) {
+      alert('¡Asombroso! ¡Más de 1000 clics! ¿Estás intentando romper el internet con tus clics?');
+    }
+  }
 }
